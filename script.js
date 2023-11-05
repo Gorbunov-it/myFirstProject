03917c1f7ab28f724c9ca5f1a8fefaac8030d3ea
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-11-05 20:57:44 +0400

    создать отдельную ветку lessons01 с файлами index.html и script.js и выполнил задание первого урока

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Создать переменных
+let title = "modal window is done";
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive = "console log is done";
 
+alert(title);
+console.log(adaptive);
