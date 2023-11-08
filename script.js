0c948368fc4b291d8e8348d5d3a5e2e5eca1959b
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-11-09 00:39:57 +0400

    Создал отдельную ветку lesson02

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // Создать переменных
-let title = "modal window is done";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive = "console log is done";
+let title = "My First Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1024;
+let rollback = 64;
+let fullPrice = 120000;
+let adaptive = true;
 
-alert(title);
-console.log(adaptive);
+// Вывод типа значений переменых
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+// Вычисление длины строки
+let lenScreens = screens.length;
+// Вывод переменной
+console.log(lenScreens);
+
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+
+// Перевод строки к нижнему регистру
+let strScreens = screens.toLowerCase();
+// Вывод массива
+console.log(strScreens.split(','));
+
+let rollPec = (fullPrice * (rollback/100));
+console.log("Процент отката: " + rollPec);
