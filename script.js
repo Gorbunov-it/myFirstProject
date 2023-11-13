844d78b7e4560ac6bfbad20782ae5c6ce996a713
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-11-13 14:41:58 +0400

    Исправление ошибок в lesson03

--- script.js
+++ script.js
@@ -21,18 +21,18 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
 // Вычисление длины строки
-let lenScreens = screens.length;
 // Вывод переменной
-console.log(lenScreens);
+console.log(screens.length);
 
-console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
 
-console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 
 // Перевод строки к нижнему регистру
-let strScreens = screens.toLowerCase();
+const strScreens = screens.toLowerCase();
 // Вывод массива
-console.log(strScreens.split(","));
+const _regExp = /\s*(?:;|$)\s*/;
+console.log(strScreens.split(_regExp));
 
 let rollPec = fullPrice * (rollback / 100);
 console.log("Процент отката: " + rollPec);
